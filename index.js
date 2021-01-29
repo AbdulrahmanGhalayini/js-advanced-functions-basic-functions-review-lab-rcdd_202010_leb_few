3b7a255e41c28d76d9114a59b7a2e782639dc331
Abdulrahman Ghalayini <ghalayini.abdulrahman.17@gmail.com>
2021-01-29 23:26:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ divide:  function  (a ,b){return a/b;}
 
 
 function actionApplyer(s, b){
+<<<<<<< HEAD
   if(b.length ===0 ){
     return s;
   }
@@ -44,5 +45,24 @@ function actionApplyer(s, b){
   return res  ;
 
   }
+=======
+  if(b = [] )
+  return s;
+  else
+  {
+   
+   let message = "13, multiplied by 2, added to 1000 and then modulo 7 is 4. Apply each function in the Array of functions on the given base (13) OR on the result of the use of the previous function to get this result"
+   let res = s;
+   for(int i= ; i<b.length; i++){
+     res= b[i](res);
+   }
+   console.log(res);
+
+return res  ;
+  }
+  
+  
+  
+>>>>>>> 279c7fc3fed33c4b06c2086b79882952f1f8e17c
 }
 
